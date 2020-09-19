bbb468cac4de81a7678d277c8c5f7c4f7b99561f
emmeem <1124782639@qq.com>
2020-09-19 22:39:09 +0800

    finish problen2 in the way3

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/controller/LevelControllerTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/controller/LevelControllerTests.java
@@ -4,17 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("qa")
+@TestPropertySource(properties = "levelNumber=1")
 public class LevelControllerTests {
-    @Autowired
-    private LevelController levelController;
 
     @Autowired
     private MockMvc mockMvc;
