e752949567be55861c71b66ffe1b459f8644d47d
emmeem <1124782639@qq.com>
2020-09-19 21:21:39 +0800

    finish problem1

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -1,37 +1,22 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
+import com.thoughtworks.capability.gtb.demospringconfig.dto.Credentials;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+import java.util.Map;
+
+@Data
 @ConfigurationProperties("mail")
 public class MailConfig {
 
     private String hostname;
     private int port;
     private String from;
-
-    public String getHostname() {
-        return hostname;
-    }
-
-    public void setHostname(String hostname) {
-        this.hostname = hostname;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
+    private List<String> defaultRecipients;
+    private Map<String,Boolean> additionalHeaders;
+    private Credentials credentials;
 
     @Override
     public String toString() {
@@ -39,6 +24,13 @@ public class MailConfig {
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
+                ",defaultRecipients[0]='" + defaultRecipients.get(0) + '\'' +
+                ",defaultRecipients[1]='" + defaultRecipients.get(1) + '\'' +
+                ",additionalHeaders.redelivery=" + additionalHeaders.get("redelivery") +
+                ",additionalHeaders.secure=" + additionalHeaders.get("secure") + '\'' +
+                ",credentials.username='" + credentials.getUsername() + '\'' +
+                ",credentials.password='" + credentials.getPassword() + '\'' +
+                ",credentials.authMethod='" + credentials.getAuthMethod() + '\'' +
                 '}';
     }
 }
---
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/dto/Credentials.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.capability.gtb.demospringconfig.dto;
+
+import lombok.Data;
+
+@Data
+public class Credentials {
+    private String username;
+    private String password;
+    private String authMethod;
+
+
+}
