a792b1838018760e8c85aa2cf9929e66e56313ba
emmeem <1124782639@qq.com>
2020-09-19 22:33:42 +0800

    finish problem2 in the way2

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/controller/LevelControllerTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/controller/LevelControllerTests.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("qa")
 public class LevelControllerTests {
     @Autowired
     private LevelController levelController;
@@ -19,22 +20,8 @@ public class LevelControllerTests {
     private MockMvc mockMvc;
 
     @Test
-    public void shouldReturnBasicWhenLevelNumberEqual0() throws Exception {
-        ReflectionTestUtils.setField(levelController, "levelNumber", 0);
-        mockMvc.perform(get("/level"))
-                .andExpect(content().string("basic"));
-    }
-
-    @Test
     public void shouldReturnAdvancedWhenLevelNumberEqual1() throws Exception {
-        ReflectionTestUtils.setField(levelController, "levelNumber", 1);
-        mockMvc.perform(get("/level"))
-                .andExpect(content().string("advanced"));
-    }
 
-    @Test
-    public void shouldReturnAdvancedWhenLevelNumberEqual2() throws Exception {
-        ReflectionTestUtils.setField(levelController, "levelNumber", 2);
         mockMvc.perform(get("/level"))
                 .andExpect(content().string("advanced"));
     }
